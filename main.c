eca8a6bfa64940faeebec210e679e21cc53ffe87
Sadesm <sadesm637@gmail.com>
2025-09-19 15:01:29 +0330

    grid avaliye

--- main.c
+++ main.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include "raylib.h"
 
+const int Height = 800;
+const int Width = 600;
+const int Row = 12;
+const int Column = 9;
+
 int main(int argc, char** argv){
   
-  InitWindow(800, 600, "Salam Donya");
+  InitWindow(Width, Height, "Salam Donya");
   SetTargetFPS(60);
   
   while(!WindowShouldClose()){
     BeginDrawing();
     ClearBackground(LIME);
-    DrawText("Slaaaaaaaaaam!??", 380, 300, 20, BLACK);
+    DrawRectangle(10, 10, Width - 20, Height - 20, SKYBLUE);
+    for(int i = 0; i < Row; i++)
+      for(int j = 0; j < Column; j++){
+        float celSizeH = (Height - 30) / Row - 10;
+        float celSizeW = (Width - 30) / Column - 10;
+
+        float posX = (celSizeH + 10) * i + 20;  
+        float posY = (celSizeW + 10) * j + 20;
+
+        Vector2 celSize = {celSizeW, celSizeH};
+        Vector2 pos = {posY, posX};
+
+        DrawRectangleV(pos, celSize, RAYWHITE);
+      }
     EndDrawing();
   
   }
